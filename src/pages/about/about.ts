143b82b0cc750782770a79a9f659a5c307d59eea
Mayrcon <mayrcon_marlon@hotmail.com>
2017-07-27 16:39:12 -0300

    Listar Jogadores do 3 times Listar jogos

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { HttpModule } from '@angular/http';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +10,15 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+jogos: any;
 
+  constructor(public navCtrl: NavController, public http: Http, public provider: HttpModule) {
+this.http.get('http://localhost/newinter2017/api/jogos.php')
+	.map(res => res.json())
+	.subscribe(data => {
+        this.jogos = data;
+        console.log(data);
+    });
   }
 
 }
