3abe6288eec28450e9451cbd897c21f6ec2306e0
mayrcon_marlon@hotmail.com <Sw880120>
2017-08-03 02:04:30 -0300

    Mudar nome de pastas e arquivos para nomes certos

---
+++ src/pages/artilharia/artilharia.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { HttpModule } from '@angular/http';
+
+@Component({
+  selector: 'page-artilharia',
+  templateUrl: 'artilharia.html'
+})
+export class ArtilhariaPage {
+
+artilharia: any;
+
+  constructor(public navCtrl: NavController, public http: Http, public provider: HttpModule) {
+
+	this.http.get('http://altinao2017.mayrcon.com.br/api/artilharia.php')
+	.map(res => res.json())
+	.subscribe(data => {
+        this.artilharia = data;
+        console.log(data);
+    });
+  }
+
+}
---
+++ src/pages/jogos/jogos.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { HttpModule } from '@angular/http';
+
+@Component({
+  selector: 'page-jogos',
+  templateUrl: 'jogos.html'
+})
+export class JogosPage {
+
+jogos: any;
+
+  constructor(public navCtrl: NavController, public http: Http, public provider: HttpModule) {
+	this.http.get('http://altinao2017.mayrcon.com.br/api/jogos.php')
+	.map(res => res.json())
+	.subscribe(data => {
+        this.jogos = data;
+        console.log(data);
+    });
+  }
+
+}
